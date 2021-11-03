43eec59b02c4a4daefef78a90a4314f2a1b9381e
Yann P <yann.pomie@laposte.net>
2021-11-03 16:47:07 +0100

    removed long flags for efficiency

--- src/fr/ird/Main.java
+++ src/fr/ird/Main.java
@@ -1,21 +1,27 @@
 package fr.ird;
 
+import fr.ird.runtime.ServerInstance;
+
+import java.io.IOException;
+
 public class Main {
 
     private static void printHelp() {
-        System.out.println("usage : rfr command");
+        System.out.println("usage : rfr command [options]");
         System.out.println("\ncommand:");
         System.out.println("    run : run an instance of the server");
         System.out.println("    version : print server's version");
         System.out.println("    help : print this help");
-
+        System.out.println("\noptions depends from command, use -h to get more information about said command");
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         switch (args[0]) {
             case "run" :
                 System.out.println("Running!");
+                ServerInstance server = new ServerInstance(args);
+                System.out.println(server.toString());
                 break;
 
             case "version" :
