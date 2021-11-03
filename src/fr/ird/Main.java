d1a87cbe98deb44d0595e5e2d40707167aff8d85
Yann P <yann.pomie@laposte.net>
2021-11-03 14:27:05 +0100

    took out brackets from Main.printHelp()

--- src/fr/ird/Main.java
+++ src/fr/ird/Main.java
@@ -3,7 +3,7 @@ package fr.ird;
 public class Main {
 
     private static void printHelp() {
-        System.out.println("usage : rfr [command]");
+        System.out.println("usage : rfr command");
         System.out.println("\ncommand:");
         System.out.println("    run : run an instance of the server");
         System.out.println("    version : print server's version");
