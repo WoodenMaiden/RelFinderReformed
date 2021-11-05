b795750d003fe18e50f2fab65a3d9f0dca02bfb0
Yann P <yann.pomie@laposte.net>
2021-11-05 16:20:44 +0100

    added JSON support from https://github.com/stleary/JSON-java, threads are now able to get data from client

--- src/fr/ird/runtime/Job.java
+++ src/fr/ird/runtime/Job.java
@@ -2,6 +2,9 @@ package fr.ird.runtime;
 
 import java.io.*;
 import java.net.Socket;
+import java.net.URLDecoder;
+import java.util.Map;
+import org.json.*;
 
 public class Job implements Runnable{
 
@@ -19,13 +22,35 @@ public class Job implements Runnable{
             BufferedReader in = new BufferedReader(new InputStreamReader(sockClient.getInputStream()));
             PrintWriter out = new PrintWriter(sockClient.getOutputStream());
 
-            System.out.println(in);
+            //int i = 0;
+            /*
+            while (i < 14){
+                System.out.println(in.readLine());
+                ++i;
+            }
+            */
 
+            String GetArgument = in.readLine().split(" ")[1].substring(1);
+
+            System.out.println(GetArgument);
+            System.out.println(URLDecoder.decode(GetArgument));
+
+
+
+            //we are doing this because the code below blocks
+            //while ((line = in.readLine()) != null){
+            //    System.out.println(line);
+            //}
             //TODO
+
+            Map <String, Object> nodes = new JSONObject(GetArgument).toMap();
+
+
+
 
 
             out.println("HTTP/1.0 200 OK");
-            out.println("Content-Type: text/html");
+            out.println("Content-Type: application/sparql-results+json");
             out.println("Access-Control-Allow-Origin: null");
             out.println("");
             out.flush();
