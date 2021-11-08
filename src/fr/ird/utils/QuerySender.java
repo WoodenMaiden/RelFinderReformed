aa6d7e8baf831de7c790266fd765f32e6d9005ae
Yann P <yann.pomie@laposte.net>
2021-11-08 16:46:29 +0100

    changed the http method to GET, made it non static to avoid synchronization issues and finally got it working

--- src/fr/ird/utils/QuerySender.java
+++ src/fr/ird/utils/QuerySender.java
@@ -1,27 +1,63 @@
 package fr.ird.utils;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
+import java.io.*;
+import java.net.*;
 import java.nio.charset.StandardCharsets;
+import org.json.JSONObject;
 import java.util.Map;
 
 public class QuerySender {
 
     //change this whether you are in production or not
-    private static final boolean isProd = false;
-    private static final String address = (isProd) ? "http://127.0.0.1:9090" : "http://sparql.southgreen.fr/";
+    private HttpURLConnection http;
 
+    public QuerySender() throws Exception {
+    }
 
     /**
      * @param query NOT FORMATTED PLEASE
      * @return Map<String, Object> equivalent to org.json.JsonObject.toMap()
      */
-    public static Map<String, Object> sendQuery(String query) throws UnsupportedEncodingException {
+    public Map<String, Object> sendQuery(String query) throws IOException {
 
         String encoded = URLEncoder.encode(query, StandardCharsets.UTF_8.name());
+        String getArguments = "?query=" + encoded + "&format=application%2Fsparql-results%2Bjson&timeout=20000";
 
-        System.out.println(encoded);
+        http = (HttpURLConnection) new URL("http://sparql.southgreen.fr/" + getArguments).openConnection();
+        http.setRequestMethod("GET");
+        http.setRequestProperty("Accept", "application/sparql-results+json,*/*;q=0.9");
+        http.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+//        http.setConnectTimeout(20000);
+//        http.setReadTimeout(20000);
+        http.setDoOutput(true);
+        http.connect();
 
-        return null;
+        DataOutputStream out = new DataOutputStream(http.getOutputStream());
+        BufferedReader in = new BufferedReader(new InputStreamReader(http.getInputStream()));
+
+        System.out.println(getArguments);
+
+        out.writeBytes(getArguments);
+        out.flush();
+        out.close();
+
+        int r;
+        if ((r = http.getResponseCode()) > 400) throw new ProtocolException("sendQuery() An error has occurred : " + r);
+
+        System.out.println("Response is : " + http.getResponseMessage());
+
+        String line;
+        StringBuilder response = new StringBuilder();
+
+        while ((line = in.readLine()) != null) {
+            response.append(line);
+        }
+
+        System.out.println(response.toString());
+
+
+        http.disconnect();
+
+        return new JSONObject(response).toMap();
     }
 }
